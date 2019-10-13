af6e855c2ff203c9b4e50b204005a5d3383fffad
Learus <jmaliaras@gmail.com>
2019-10-13 22:17:31 +0300

    Updates

---
+++ precache-manifest.9cbaf1c2094c8551b5bef491400cadff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2867478cbbf93177f89b2c5d8c0f4067",
+    "url": "/react-material-ui-carousel/index.html"
+  },
+  {
+    "revision": "5d420176df8f4d5584b4",
+    "url": "/react-material-ui-carousel/static/css/main.7ae0dd6e.chunk.css"
+  },
+  {
+    "revision": "36960efb56c4b3952115",
+    "url": "/react-material-ui-carousel/static/js/2.4a7ff272.chunk.js"
+  },
+  {
+    "revision": "5d420176df8f4d5584b4",
+    "url": "/react-material-ui-carousel/static/js/main.8a4ebed4.chunk.js"
+  },
+  {
+    "revision": "d218b74495ba0c79a5f9",
+    "url": "/react-material-ui-carousel/static/js/runtime-main.464c6120.js"
+  }
+]);
