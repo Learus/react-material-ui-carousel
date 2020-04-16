9e00d3b45d3b9422bc9c49da922b5668874ce2e6
Learus <jmaliaras@gmail.com>
2020-04-16 15:51:08 +0300

    added definitions for typescript support

---
+++ src/@types/react-material-ui-carousel/index.d.ts
@@ -0,0 +1,15 @@
+
+    import React, { ReactNode } from 'react';
+
+    export interface CarouselProps {
+        indicators?: boolean,
+        autoPlay?: boolean,
+        interval?: number,
+        animation?: 'fade' | 'slide',
+        children?: ReactNode,
+        className: string
+    }
+    
+    declare const Carousel: React.ComponentType<CarouselProps>;
+    
+    export default Carousel;
