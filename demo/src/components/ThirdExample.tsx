21c56f864366258b85536cd596b430b74ecb3a4c
Learus <jmaliaras@gmail.com>
2022-03-25 21:50:51 +0200

    removed next/prev tests from demo

--- demo/src/components/ThirdExample.tsx
+++ demo/src/components/ThirdExample.tsx
@@ -10,13 +10,12 @@ export default function Test3()
     const [height3, setHeight3] = useState(400);
 
     const heights = [height1, height2, height3];
-    const [index, setIndex] = useState<number>(0);
 
     return (
         <div className="Example3" style={{ marginTop: "50px", color: "#494949" }}>
             <Typography variant='h4'>Example: Dynamic height</Typography>
             <br />
-            <Carousel animation="fade" navButtonsAlwaysVisible autoPlay={false} index={index}>
+            <Carousel animation="fade" navButtonsAlwaysVisible autoPlay={false}>
                 {
                     heights.map((item, i) => (
                         <Paper elevation={10} style={{ height: item }} className="HeightItem">
@@ -30,9 +29,6 @@ export default function Test3()
             <TextField variant='outlined' value={height1} label='Height 1' onChange={(e) => setHeight1(+e.target.value)} />
             <TextField variant='outlined' value={height2} label='Height 2' onChange={(e) => setHeight2(+e.target.value)} />
             <TextField variant='outlined' value={height3} label='Height 3' onChange={(e) => setHeight3(+e.target.value)} />
-
-            <button onClick={() => setIndex(i => i + 1)}>next</button>
-            <button onClick={() => setIndex(i => i - 1)}>prev</button>
 
             <br />
             <Typography style={{ marginTop: 20 }}>
